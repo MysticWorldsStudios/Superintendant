c6f0028ceaaa8bea1cb08f66e297d137f3e0cf1e
Sir-Jitterbug <74777689+Sir-Jitterbug@users.noreply.github.com>
2020-11-27 11:02:56 +0000

    added to keep this at same area as DBH server

---
+++ commands/sellrations.js
@@ -0,0 +1,37 @@
+const db = require('quick.db')
+const ms = require('parse-ms')
+const Discord = require('discord.js')
+
+module.exports.run = async (bot, message, args) => {
+    
+    let user = message.mentions.members.first() || message.author
+    let amount = 5
+	let sale = 1
+    let author = await db.fetch(`money_${message.author.id}`) // fetch authors balance
+
+
+    if (nw < 1) { // if the authors got less than 1 return this.
+        return message.channel.send(':x: You need atleast 1 lot of Food Rations to sell!')
+    }
+
+    db.subtract(`rations_${message.author.id}`, sale)
+    db.add(`money_${user.id}`, amount)
+         
+    
+  let bal = await db.fetch(`money_${user.id}`)
+  let ra = await db.fetch(`rations_${message.author.id}`)
+  
+    let embed = new Discord.MessageEmbed()
+    .setAuthor(`Sold Successfully a NightWing!`, message.author.displayAvatarURL)
+    .addField(`Amount`, `${sale} food rations\nYou now have ${ra} Nightwings`)
+    .addField(`Balance Updated`, `${bal}C`)
+    .setColor("Green") // random = "RANDOM"
+    .setTimestamp()
+    // change args[0] to args[1] since it's much easier to do the command.
+
+    message.channel.send(embed)
+}
+module.exports.help = {
+    name: 'sell_rations',
+    aliases: ["sell_food"]
+};
