c22903964a53da315139b4c5ff739bbc27658617
Sir-Jitterbug <74777689+Sir-Jitterbug@users.noreply.github.com>
2021-11-19 09:34:43 +0000

    Deleting SI stuff from AGIS branch

--- commands/hm.js
+++
@@ -1,9 +0,0 @@
-module.exports.run = async (bot, message, args) => {
-  
-  message.channel.send("hmmmmmmmmmmmmmmmmm <:SIConfused:647918961396154387> ** *thinking intensifies* **");
-}
-
-module.exports.help = {
-  name: "hm",
-  aliases: []
-}
