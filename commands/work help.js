e1b01d4102dc63c405d3c904f57e2468586acf90
Sir-Jitterbug <74777689+Sir-Jitterbug@users.noreply.github.com>
2021-11-19 09:28:30 +0000

    Purging Old commands from the folder

--- commands/work help.js
+++
@@ -1,9 +0,0 @@
-module.exports.run = async (bot, message, args) => {
-  
-  message.channel.send("Job's Available are:\nhovbus \nprogrammer \nconstructor");
-}
-
-module.exports.help = {
-  name: "work_help",
-  aliases: []
-}
